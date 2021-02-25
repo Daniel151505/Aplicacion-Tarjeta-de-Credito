cdf17ee648d591adf108c6807babeb55e01f2736
Daniel151505 <daniel150520011@gmail.com>
2021-02-25 12:57:37 -0500

    Agregando Componente Crear Tarjeta

--- Aplicacion-Tarjeta-de-Credito/src/app/app.module.ts
+++ Aplicacion-Tarjeta-de-Credito/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearTarjetaComponent
   ],
   imports: [
     BrowserModule,
