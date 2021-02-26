4e99f3b2ee17f6cc7d2a3f37b98d0455ae6e1f6f
Daniel151505 <daniel150520011@gmail.com>
2021-02-25 21:56:17 -0500

    Importando ReactiveFormsModule

--- Aplicacion-Tarjeta-de-Credito/src/app/app.module.ts
+++ Aplicacion-Tarjeta-de-Credito/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
 import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,9 @@ import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarje
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
