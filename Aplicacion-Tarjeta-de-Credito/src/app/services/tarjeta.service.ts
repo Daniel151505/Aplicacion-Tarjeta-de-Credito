ac611e8e9ebfa51c51a44a951a13b719f8e288b9
Daniel151505 <daniel150520011@gmail.com>
2021-02-26 23:27:05 -0500

    Agregando Metodo Eliminar Tarjeta

--- Aplicacion-Tarjeta-de-Credito/src/app/services/tarjeta.service.ts
+++ Aplicacion-Tarjeta-de-Credito/src/app/services/tarjeta.service.ts
@@ -8,16 +8,20 @@ import { TarjetaCredito } from '../models/TarjetaCredito';
 })
 export class TarjetaService {
 
-  constructor(private firebase: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore) { }
 
   guardarTarjeta(tarjeta: TarjetaCredito): Promise<any> {
-    return this.firebase.collection('tarjetas').add(tarjeta)
+    return this.firestore.collection('tarjetas').add(tarjeta)
   }
 
   obtenerTarjetas(): Observable<any> {
-    return this.firebase.collection('tarjetas', ref => 
+    return this.firestore.collection('tarjetas', ref => 
           ref.orderBy('fechaCreacion','asc')
     ).snapshotChanges()
+  }
+
+  eliminarTarjeta(id: string) : Promise<any> {
+    return this.firestore.collection('tarjetas').doc(id).delete()
   }
 
 }
