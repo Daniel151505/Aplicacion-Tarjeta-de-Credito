bfe72b69ded8406cde2d72cd60542e8f6f606515
Daniel151505 <daniel150520011@gmail.com>
2021-02-26 23:17:21 -0500

    Obteniendo los datos de las tarjetas

--- Aplicacion-Tarjeta-de-Credito/src/app/components/listar-tarjeta/listar-tarjeta.component.ts
+++ Aplicacion-Tarjeta-de-Credito/src/app/components/listar-tarjeta/listar-tarjeta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { TarjetaCredito } from 'src/app/models/TarjetaCredito';
 import { TarjetaService } from 'src/app/services/tarjeta.service';
 
 @Component({
@@ -8,9 +10,24 @@ import { TarjetaService } from 'src/app/services/tarjeta.service';
 })
 export class ListarTarjetaComponent implements OnInit {
 
+  listarTarjetas: TarjetaCredito[] = []
+
   constructor(private tarjetaService: TarjetaService) { }
 
   ngOnInit(): void {
+    this.obtenerTajetas()
+  }
+
+  obtenerTajetas(){
+    this.tarjetaService.obtenerTarjetas().subscribe(doc => {
+      this.listarTarjetas = []
+      doc.forEach(element => {
+        this.listarTarjetas.push({
+          id:element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      })
+    })
   }
 
 
